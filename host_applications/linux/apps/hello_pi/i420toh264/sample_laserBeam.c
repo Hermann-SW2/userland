9d4ffc0265ffcb21283e8a73f0906df4a079b3e0
Hermann-SW2 <hermann@stamm-wilbrandtd.de>
2021-01-31 14:37:16 +0100

    Determine 2-wheel robot pitch by locating dot of attached green laser in frame

---
+++ host_applications/linux/apps/hello_pi/i420toh264/sample_laserBeam.c
@@ -0,0 +1,67 @@
+// gcc -Wall -pedantic -Wextra sample_laserBeam.c -O3 -o sample_laserBeam
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+// https://www.99colors.net/name/blue
+const unsigned char y_blue =       255 * 0.11;
+const unsigned char u_blue = 128 + 255 * 0.44;
+const unsigned char v_blue = 128 - 255 * 0.10;
+
+int align_up(int x, int y)
+{
+  return ((x-1)|((1<<y)-1))+1;
+}
+
+int main(int argc, char *argv[])
+{
+  int width, height, nStride, nSliceHeight, nLenY, nLenU, nLenV, xintc, yintc;
+  unsigned char *buf, mx, fnd;
+  int x, y, s;
+  assert(argc==5 ||
+         !"Format: sample_laserBeam width height x-intercept y-intercept");
+
+  width  = atoi(argv[1]); nStride      = align_up(width,  5);
+  height = atoi(argv[2]); nSliceHeight = align_up(height, 4);
+  xintc  = atoi(argv[3]);
+  yintc  = atoi(argv[4]);
+
+  nLenY = nStride * nSliceHeight;  nLenU = nLenV = nLenY / 4;
+  assert( (buf = malloc(nLenY+nLenU+nLenV)) );
+
+  fread(buf, nLenY+nLenU+nLenV, 1, stdin);
+  while (!feof(stdin))
+  {
+    for(mx=0, y=0, x=xintc, s=0; y<=yintc; ++y)
+    {
+      s += xintc;  if (s > yintc)  { s -= yintc; --x; }
+
+      if (buf[x+y*nStride] > mx)  mx = buf[x+y*nStride];
+    }
+
+    for(fnd=0, y=0, x=xintc, s=0; y<=yintc; ++y)
+    {
+      s += xintc;  if (s > yintc)  { s -= yintc; --x; }
+
+      if (buf[x+y*nStride] == mx) 
+      {
+        buf[x+y*nStride]                             = y_blue;
+        buf[(x/2)+(y/2)*(nStride/2) + nLenY]         = u_blue;
+        buf[(x/2)+(y/2)*(nStride/2) + nLenY + nLenU] = v_blue;
+        fnd = 1;
+      }
+      else if (fnd)
+        break; 
+    }
+    // fprintf(stderr, "x=%d y=%d s=%d\n", x, y, s);
+
+    fwrite(buf, nLenY+nLenU+nLenV, 1, stdout); fflush(stdout);
+
+    fread(buf, nLenY+nLenU+nLenV, 1, stdin);
+  }
+
+  free(buf);
+
+  return 0;
+}
